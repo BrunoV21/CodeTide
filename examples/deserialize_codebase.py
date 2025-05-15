57ba6387260c0d6bd067dc5dfb697f0ebb8abee0
BrunoV21 <bruno.vitorino@tecnico.ulisboa.pt>
2025-05-15 21:54:24 +0100

    Fix print statements in deserialize_codebase.py to use correct method names for file and module tree structures

--- examples/deserialize_codebase.py
+++ examples/deserialize_codebase.py
@@ -1,9 +1,16 @@
+# Add the root directory to sys.path if needed
+# This might be needed if the codetide package isn't installed
+import sys
+import os
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from codetide.core.models import CodeBase
 from codetide.tide import CodeTide
 
 codeBase = CodeBase.deserialize()
 tide = CodeTide(codebase=codeBase)
 
-print(tide.get_file_tree_structure())
-
-print(tide.get_module_function_tree())
+print(tide.get_files_tree())
+print("\n\n")
+print(tide.get_modules_tree())
