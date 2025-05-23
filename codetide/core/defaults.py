84e93d269be3489644f9de8fe15a826deff3d124
BrunoV21 <bruno.vitorino@tecnico.ulisboa.pt>
2025-05-23 23:06:59 +0100

    Remove unused gitignore definitions and clean up default settings in defaults.py

--- codetide/core/defaults.py
+++ codetide/core/defaults.py
@@ -3,13 +3,6 @@ import os
 
 INSTALLATION_DIR = Path(os.path.abspath(os.path.dirname(__file__)))
 
-GITINGORE_FILE = ".gitignore"
-
-DEFAULT_GITIGNORE = [
-    ".git/",
-    "*.pyc"
-]
-
 # Language extensions mapping
 LANGUAGE_EXTENSIONS = {
     'python': ['.py'],
@@ -25,43 +18,36 @@ LANGUAGE_EXTENSIONS = {
     'php': ['.php'],
     'csharp': ['.cs'],
     'kotlin': ['.kt', '.kts'],
-    'scala': ['.scala']
+    'scala': ['.scala'],
+    # Web and templates
+    'html': ['.html', '.htm', '.xhtml', '.html5'],
+    'css': ['.css', '.scss', '.sass', '.less', '.styl'],
+    'xml': ['.xml', '.xsd', '.xsl', '.xslt', '.rss', '.svg', '.svgz'],
+    'yaml': ['.yaml', '.yml'],
+    'json': ['.json', '.json5', '.jsonl', '.geojson', '.topojson', '.jsonc'],
+    'markdown': ['.md', '.markdown', '.mdown', '.mdwn', '.mkd', '.mkdn'],
+    'jinja': ['.j2', '.jinja', '.jinja2'],
+    # Configuration files
+    'config': [
+        '.ini', '.cfg', '.conf', '.properties', '.toml', 
+        '.env', '.env.local', '.env.dev', '.env.prod'
+    ],
+    # Documentation
+    'documentation': [
+        '.txt', '.text',
+        '.tex', '.bib'
+    ],
+    # Container and deployment
+    'container': [
+        'Dockerfile', 'docker-compose.yml',
+        'docker-compose.yaml', '.dockerignore'
+    ]
+    
 }
 
 DEFAULT_MAX_CONCURRENT_TASKS = 50
 DEFAULT_BATCH_SIZE = 128
 
-# Default file patterns to ignore
-DEFAULT_IGNORE_PATTERNS = [
-    '*.pyc', '*.pyo', '*.so', '*.o', '*.a', '*.lib', '*.dll', '*.exe',
-    '*.jar', '*.war', '*.ear', '*.zip', '*.tar', '*.gz', '*.bz2', '*.7z',
-    '*.egg', '*.egg-info', '*.whl',
-    '__pycache__', '.git', '.hg', '.svn', '.DS_Store', 
-    'node_modules', 'venv', 'env', '.env', '.venv',
-    'build', 'dist', 'target', '.idea', '.vscode'
-]
-
-
-DEFAULT_REGEX_DEFS_PATH = INSTALLATION_DIR / "parsers" / "regex" / "regex_definitions"
-
-DEFAULT_TEMPLATES_PATH = INSTALLATION_DIR / "parsers" / "templates"
-
 DEFAULT_ENCODING = "utf8"
 
 DEFAULT_ENCODINGS = ['utf-8', 'latin1', 'cp1252']
-
-LOGGING_DIR = "./logs/"
-
-SERIALIZATION_DIR = "./storage"
-
-SERIALIZED_GRAPH = "graph.json"
-
-SERIALIZED_CODEBASE = "codebase.json"
-
-SERIALIZED_CLASSFUNCREPO = "knowledge.json"
-
-SERIALZIED_PARSER_OBJ = "parser.json"
-
-SERIALIZED_ANNOTATIONS = "annotations.json"
-
-SERIALIZED_AICORE_LLM_CONFIG = "aicore_llm_config.json"
