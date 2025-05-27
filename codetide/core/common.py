e98e43544a4b458494078db47cf0d4d57ec0cf5a
BrunoV21 <bruno.vitorino@tecnico.ulisboa.pt>
2025-05-27 23:56:42 +0100

    Add wrap_content function to format content with safe file paths

--- codetide/core/common.py
+++ codetide/core/common.py
@@ -1,3 +1,4 @@
+import re
 from codetide.core.defaults import DEFAULT_ENCODING
 from typing import Union
 from pathlib import Path
@@ -10,3 +11,9 @@ def readFile(path :Union[str, Path], mode :str="r")->str:
 def writeFile(contents :str, path :Union[str, Path], mode :str="w"):
     with open(path, mode, encoding=DEFAULT_ENCODING) as  _file:
         _file.write(contents)
+
+def wrap_content(content: str, filepath: str) -> str:
+    safe_path = re.sub(r"[^\w\-\.]", "_", filepath)
+    return f"""<FILE_START::{safe_path}>
+{content}
+</FILE_END::{safe_path}>"""
