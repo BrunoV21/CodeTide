401a218cf0756350aa91306cc149151e1d4705b3
BrunoV21 <bruno.vitorino@tecnico.ulisboa.pt>
2025-06-30 23:18:18 +0100

    Refactor agent_tide.py to improve structure and readability; separate LLM initialization and message trimming into distinct functions

--- examples/agent_tide.py
+++ examples/agent_tide.py
@@ -4,10 +4,12 @@ Make sure to set `CODETIDE_WORKSPACE` env var which will be requried for the MCP
 """
 
 from aicore.llm import Llm, LlmConfig
+from aicore.logger import _logger
 from codetide.mcp import codeTideMCPServer
+from typing import Optional
 import os
 
-async def main():
+def init_llm()->Llm:
     llm = Llm.from_config(
         LlmConfig(
             model="deepseek-chat",
@@ -18,12 +20,42 @@ async def main():
     )
     
     llm.provider.mcp.add_server(name=codeTideMCPServer.name, parameters=codeTideMCPServer)
-    await llm.acomplete(
-        prompt="How many parsers are currently supported?",
-        system_prompt=["You are a software engineer with tool calling capabilities who will help the user navigate his codebase"],
-        agent_id="agent-tide",
-        action_id="warm-up"
-    )
+    return llm
+
+def trim_messages(messages, tokenizer_fn, max_tokens :Optional[int]=None):
+    max_tokens = max_tokens or int(os.environ.get("MAX_HISTORY_TOKENS", 1028))
+    while messages and sum(len(tokenizer_fn(msg)) for msg in messages) > max_tokens:
+        messages.pop(0)  # Remove from the beginning
+
+AGENT_TIDE_SYSTEM_PROMPT = """
+You are a software engineer with tool calling capabilities who will help the user navigate his codebase
+"""
+
+async def main(max_tokens :int=48000):
+    llm = init_llm()
+    history = []
+
+    _logger.logger.info("Agent is ready. Press ESC to exit.\n")
+    try:
+        while True:
+            try:
+                message = input("You: ").strip()
+                if not message:
+                    continue
+            except EOFError:
+                break  # Ctrl+D on Unix, Ctrl+Z on Windows
+            except KeyboardInterrupt:
+                _logger.logger.warning("\nExiting...")
+                break
+
+            history.append(message)
+            trim_messages(history, llm.tokenizer, max_tokens)
+
+            response = await llm.acomplete(history, system_prompt=[AGENT_TIDE_SYSTEM_PROMPT])
+            history.append(response)
+            
+    except KeyboardInterrupt:
+        _logger.logger.warning("\nExited by user.")
 
 if __name__ == "__main__": 
     from dotenv import load_dotenv
@@ -31,5 +63,3 @@ if __name__ == "__main__":
 
     load_dotenv()
     asyncio.run(main())
-
-
