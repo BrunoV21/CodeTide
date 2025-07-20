5f764579ce77c041fd6f6902897b935dea3472cd
BrunoV21 <bruno.vitorino@tecnico.ulisboa.pt>
2025-07-20 12:47:09 +0100

    updated server with serve and serve async methods

--- codetide/mcp/server.py
+++ codetide/mcp/server.py
@@ -2,6 +2,16 @@
 from fastmcp import FastMCP
 
 codeTideMCPServer = FastMCP(
-    name="CodeTide",
+    name="codetide",
     # instructions=AGENT_TIDE_SYSTEM_PROMPT,
 )
+
+def serve():
+    codeTideMCPServer.run("stdio")
+
+async def aserve():
+    await codeTideMCPServer.run_stdio_async()
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(serve())
