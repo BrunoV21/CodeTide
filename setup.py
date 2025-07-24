7c719757751fc3310706158a5fce4e9d5c4e7843
BrunoV21 <bruno.vitorino@tecnico.ulisboa.pt>
2025-07-24 23:30:03 +0100

    added requirements-agents-ui dependency

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 requirements = (here / "requirements.txt").read_text(encoding="utf-8").splitlines()
 requirements_visualization = (here / "requirements-visualization.txt").read_text(encoding="utf-8").splitlines()
 requirements_agents = (here / "requirements-agents.txt").read_text(encoding="utf-8").splitlines()
+requirements_agents_ui = (here / "requirements-agents-ui.txt").read_text(encoding="utf-8").splitlines().extend(requirements_agents)
 
 setup(
     name="codetide",
@@ -21,7 +22,8 @@ setup(
     include_package_data=True,
     extras_require={
         "visualization": requirements_visualization,
-        "agents": requirements_agents
+        "agents": requirements_agents,
+        "agents-ui": requirements_agents_ui
     },
     entry_points={
         "console_scripts": [
